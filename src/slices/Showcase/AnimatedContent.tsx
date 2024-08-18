c393711428c9f8827146e98fef0759af35b1fd9d
Tim Schulz <tim.schulz.rc@gmail.com>
2024-08-18 17:28:52 +0200

    add animation to showcase

---
+++ src/slices/Showcase/AnimatedContent.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import usePrefersReducedMotion from "@/hooks/usePrefersReducedMotion";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useRef } from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function AnimatedContent({ children }: Props) {
+  const container = useRef(null);
+  const preferesReducedMotion = usePrefersReducedMotion();
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(
+    () => {
+      if (preferesReducedMotion) {
+        return gsap.set(container.current, { y: 0 });
+      }
+      gsap.fromTo(
+        container.current,
+        { y: 100 },
+        {
+          y: 0,
+          ease: "power2.inOut",
+          duration: 1,
+          scrollTrigger: {
+            trigger: container.current,
+            start: "top bottom-=40%",
+            toggleActions: "play pause resume reverse",
+          },
+        },
+      );
+    },
+    { scope: container },
+  );
+
+  return <div ref={container}>{children}</div>;
+}
